9d9f6c69043b781decd2413ad86d61a20de33ba7
陈海飞 <649719596@qq.com>
2025-03-31 15:31:09 +0800

    feat: 添加表格快捷键映射，优化表格操作体验

--- demo/demo.ts
+++ demo/demo.ts
@@ -1,6 +1,4 @@
 import 'prosemirror-view/style/prosemirror.css'
-import 'prosemirror-menu/style/menu.css'
-import 'prosemirror-example-setup/style/style.css'
 import 'prosemirror-gapcursor/style/gapcursor.css'
 import '../style/tables.css'
 import '../style/table-header-menu.css'
@@ -13,8 +11,10 @@ import { EditorState } from 'prosemirror-state'
 import { DOMParser, Schema } from 'prosemirror-model'
 import { schema as baseSchema } from 'prosemirror-schema-basic'
 import { keymap } from 'prosemirror-keymap'
+import { baseKeymap } from 'prosemirror-commands'
+import { history, undo, redo } from 'prosemirror-history'
 
-import { goToNextCell } from '../src'
+import { goToNextCell, addColumnAfter, addColumnBefore, deleteColumn, addRowAfter, addRowBefore, deleteRow } from '../src'
 import { tableEditing, columnResizing, tableNodes, fixTables } from '../src'
 import { tableHeaderMenuPlugin } from '../src/tableHeaderMenu'
 import { tableOverlayPlugin } from '../src/tableOverlayPlugin'
@@ -39,28 +39,56 @@ const schema = new Schema({
       },
     }),
   ),
+  marks: baseSchema.spec.marks,
 })
 
+// 创建表格快捷键映射
+const tableKeymap = {
+  // 行列操作
+  'Alt-ArrowUp': addRowBefore,
+  'Alt-ArrowDown': addRowAfter,
+  'Alt-ArrowLeft': addColumnBefore,
+  'Alt-ArrowRight': addColumnAfter,
+  'Alt-Backspace': deleteRow,
+  'Alt-Delete': deleteColumn,
+
+  // 表格内导航
+  Tab: goToNextCell(1),
+  'Shift-Tab': goToNextCell(-1),
+
+  // 编辑操作快捷键映射
+  'Mod-z': undo,
+  'Mod-y': redo,
+  'Mod-Shift-z': redo,
+}
+
 const contentElement = document.querySelector('#content')
-if (!contentElement) throw new Error('Failed to find #content')
+if (!contentElement) throw new Error('找不到 #content 元素')
 
 const doc = DOMParser.fromSchema(schema).parse(contentElement)
 
 let state = EditorState.create({
   doc,
   plugins: [
+    // 基础编辑功能
+    history(),
+    keymap(baseKeymap),
+
+    // 表格功能插件
     columnResizing(),
     tableHeaderMenuPlugin(),
     tableOverlayPlugin(),
     tableAddCellButtonPlugin(),
     tableCellButtonPlugin(),
+
+    // 表格快捷键在tableEditing前添加
+    keymap(tableKeymap),
+
+    // 表格编辑插件放在最后
     tableEditing(),
-    keymap({
-      Tab: goToNextCell(1),
-      'Shift-Tab': goToNextCell(-1),
-    }),
   ],
 })
+
 const fix = fixTables(state)
 if (fix) state = state.apply(fix.setMeta('addToHistory', false))
 
